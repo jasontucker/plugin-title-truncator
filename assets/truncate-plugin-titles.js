50e9a423060dfd9d8a587e5b5f20da62255e561c
Jason Tucker <jason@tucker.pro>
2023-04-07 14:25:17 -0700

    Update truncate-plugin-titles.js
    Prompt -  Since the image for the plugin is in the same div as the plugin title when you are truncating it you are also removing the thumbnail image, can you exclude img tags from the truncation?
    
    ChatGTP -
    My apologies for the oversight. To ensure the thumbnail image is not affected during the truncation process, we can update the truncate-plugin-titles.js file to only modify the text content of the plugin title without altering any child elements.
    
    Here's the updated JavaScript code for the truncate-plugin-titles.js file:

--- assets/truncate-plugin-titles.js
+++ assets/truncate-plugin-titles.js
@@ -13,11 +13,11 @@ function truncatePluginTitlesInSearchResults() {
     const pluginTitles = document.querySelectorAll('.plugin-card h3');
 
     pluginTitles.forEach((title) => {
-        const originalTitle = title.textContent;
+        const originalTitle = title.childNodes[0].textContent;
         const truncatedTitle = truncateTitleToNWords(originalTitle);
 
         if (originalTitle !== truncatedTitle) {
-            title.textContent = truncatedTitle;
+            title.childNodes[0].textContent = truncatedTitle;
         }
     });
 }
